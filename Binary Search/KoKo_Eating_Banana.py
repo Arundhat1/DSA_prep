d04823d3f5e7da22526b88cd3f3643432357642c
Arundhat1 <149229660+Arundhat1@users.noreply.github.com>
2025-10-12 11:09:41 +0530

    Create KoKo_Eating_Banana.py

---
+++ Binary Search/KoKo_Eating_Banana.py
@@ -0,0 +1,22 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low, high = 1, max(piles)
+        
+        def can_finish(speed):
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p / speed)
+            return hours <= h
+        
+        while low < high:
+            mid = (low + high) // 2
+            if can_finish(mid):
+                high = mid
+            else:
+                low = mid + 1
+        
+        return low
+
+        
