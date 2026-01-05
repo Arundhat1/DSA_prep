9b5ac8b6ad7edce5e0c30a16fcf5f46faab28827
Arundhat1 <149229660+Arundhat1@users.noreply.github.com>
2026-01-05 11:59:51 +0530

    Implement Morris Inorder Traversal for Binary Tree

---
+++ Binary Tree/MorrisInorder.py
@@ -0,0 +1,21 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        inorder = []
+        curr  = root
+        while curr:
+            if not curr.left:
+                inorder.append(curr.val)
+                curr = curr.right
+            else:
+                prev = curr.left
+                while prev.right and prev.right != curr:
+                    prev = prev.right
+                if not prev.right:
+                    prev.right = curr
+                    curr = curr.left
+                else:
+                    
+                    prev.right = None
+                    inorder.append(curr.val)
+                    curr = curr.right
+        return inorder
